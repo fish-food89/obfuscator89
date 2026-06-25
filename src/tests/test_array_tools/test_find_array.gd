41fec670f161a328c5755368a16c4ba70dbbb1e1
fish_food89 <fishfoodeightynine@gmail.com>
2026-06-25 11:38:34 +0300

    Added basic tests for the WHAT_CUT_SHORT error
    Some smarter test cases are still needed.

--- src/tests/test_array_tools/test_find_array.gd
+++ src/tests/test_array_tools/test_find_array.gd
@@ -34,19 +34,19 @@ var params__previous_error__ok: Array = self.ParameterFactory.named_parameters(
             [1, 2, 3],
             [1],
             1,
-            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND),
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
         ],
         [
             [1, 2, 3],
             [4],
             0,
-            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND),
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
         ],
         [
             [1, 2, 3],
             [1],
             5,
-            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND),
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
         ],
         [
             ["F", "o", "o", "B", "a", "r", "B", "a", "z"],
@@ -59,6 +59,79 @@ var params__previous_error__ok: Array = self.ParameterFactory.named_parameters(
             ["a", "r"],
             1,
             FindArrayResult.new(Error89.Code.OK, 4),
+        ],
+        [
+            ["F", "o", "o", "B", "a", "r", "B", "a", "z"],
+            ["h", "i"],
+            0,
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
+        ],
+        [
+            ["F", "o", "o", "B", "a", "r", "B", "a", "z"],
+            ["B", "a", "r"],
+            9,
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
+        ],
+    ],
+)
+
+var params__previous_error__what_cut_short: Array = self.ParameterFactory.named_parameters(
+    # names
+    [
+        "source",
+        "what",
+        "from",
+        "expected_result",
+    ],
+    # values
+    [
+        [
+            [1, 2, 3],  # source
+            [1],  # what
+            0,  # from
+            FindArrayResult.new(Error89.Code.OK, 0),  # expected_result
+        ],
+        [
+            [1, 2, 3],
+            [2],
+            0,
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
+        ],
+        [
+            [1, 2, 3],
+            [3],
+            0,
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
+        ],
+        [
+            [1, 2, 3],
+            [1],
+            1,
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
+        ],
+        [
+            [1, 2, 3],
+            [4],
+            0,
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
+        ],
+        [
+            [1, 2, 3],
+            [1],
+            5,
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
+        ],
+        [
+            ["F", "o", "o", "B", "a", "r", "B", "a", "z"],
+            ["B", "a", "r"],
+            0,
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
+        ],
+        [
+            ["F", "o", "o", "B", "a", "r", "B", "a", "z"],
+            ["a", "r"],
+            1,
+            FindArrayResult.new(Error89.Code.WHAT_NOT_FOUND, -1),
         ],
         [
             ["F", "o", "o", "B", "a", "r", "B", "a", "z"],
@@ -84,6 +157,24 @@ func test_previous_error__ok(params=self.use_parameters(params__previous_error__
         params.what,
         params.from,
         Error89.Code.OK,
+    )
+
+    self.assert_is(result, FindArrayResult, "Wrong child class!")
+    self.assert_eq(result.get_class_name(), params.expected_result.get_class_name())
+    self.assert_eq(result.error, params.expected_result.error)
+    self.assert_eq(result.index, params.expected_result.index)
+
+
+## Tests the behaviour when the `previous_error` argument is given the value
+## `Error89.Code.WHAT_CUT_SHORT`.
+func test_previous_error__what_cut_short(
+        params=self.use_parameters(params__previous_error__what_cut_short),
+) -> void:
+    var result: FindArrayResult = ArrayTools.find_array(
+        params.source,
+        params.what,
+        params.from,
+        Error89.Code.WHAT_CUT_SHORT,
     )
 
     self.assert_is(result, FindArrayResult, "Wrong child class!")
